31993bbc56382cca5c9b78cd2d00ef95ace411c3
Gansen Hu <2072806652@qq.com>
2017-11-04 14:10:21 +0800

    [Example]Add COS API usage

--- backend/src/test/java/com/fudan/cubemix/CosExample.java
+++ backend/src/test/java/com/fudan/cubemix/CosExample.java
@@ -1,4 +1,99 @@
 package com.fudan.cubemix;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.SDKGlobalConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.ibm.oauth.BasicIBMOAuthCredentials;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+
 public class CosExample {
+    private static AmazonS3 _s3Client;
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+        SDKGlobalConfiguration.IAM_ENDPOINT = "https://iam.bluemix.net/oidc/token";
+
+        String bucketName = "novemser";
+        String api_key = "4GB6RZSn4YnCd9oANgvkLaJenkbhx4JxOX6FubEKucGm";
+        String service_instance_id = "crn:v1:bluemix:public:cloud-object-storage:global:a/6b9356a9cf1bc49b2f7ea2014a764e29:d1bfd0b6-b969-4e3e-8213-900b38abba40::";
+        String endpoint_url = "https://s3-api.us-geo.objectstorage.softlayer.net";
+        String location = "us";
+
+        System.out.println("Current time: " + new Timestamp(System.currentTimeMillis()).toString());
+        _s3Client = createClient(api_key, service_instance_id, endpoint_url, location);
+        listObjects(bucketName, _s3Client);
+        listBuckets(_s3Client);
+    }
+
+    /**
+     * @param api_key
+     *            (or access key)
+     * @param service_instance_id
+     *            (or secret key)
+     * @param endpoint_url
+     * @param location
+     * @return AmazonS3
+     */
+    public static AmazonS3 createClient(String api_key, String service_instance_id, String endpoint_url, String location)
+    {
+        AWSCredentials credentials;
+        if (endpoint_url.contains("objectstorage.softlayer.net")) {
+            credentials = new BasicIBMOAuthCredentials(api_key, service_instance_id);
+        } else {
+            String access_key = api_key;
+            String secret_key = service_instance_id;
+            credentials = new BasicAWSCredentials(access_key, secret_key);
+        }
+        ClientConfiguration clientConfig = new ClientConfiguration().withRequestTimeout(5000);
+        clientConfig.setUseTcpKeepAlive(true);
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint_url, location)).withPathStyleAccessEnabled(true)
+                .withClientConfiguration(clientConfig).build();
+        return s3Client;
+    }
+
+    /**
+     * @param bucketName
+     * @param s3Client
+     */
+    public static void listObjects(String bucketName, AmazonS3 s3Client)
+    {
+        System.out.println("Listing objects in bucket " + bucketName);
+        ObjectListing objectListing = s3Client.listObjects(new ListObjectsRequest().withBucketName(bucketName));
+        for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+            System.out.println(" - " + objectSummary.getKey() + "  " + "(size = " + objectSummary.getSize() + ")");
+        }
+        System.out.println();
+    }
+
+    /**
+     * @param s3Client
+     */
+    public static void listBuckets(AmazonS3 s3Client)
+    {
+        System.out.println("Listing buckets");
+        final List<Bucket> bucketList = _s3Client.listBuckets();
+        for (final Bucket bucket : bucketList) {
+            System.out.println(bucket.getName());
+        }
+        System.out.println();
+    }
+
 }
