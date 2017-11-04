b4824fe2910e5e2c78df0b24027b6f9c8bdf8f25
naiveee-Q <15757402949@163.com>
2017-11-04 23:32:25 +0800

    Add files via upload

---
+++ fontend/object.lua
@@ -0,0 +1,53 @@
+function NewObject()
+	c = {
+		displayed = false,
+		x = 0,
+		z = 0,
+		y = 0,
+		name = "",
+		id = "",
+		text = "",
+		imageTag = "",
+		init = Object.init,
+		setInfos = Object.setInfos,
+		destroy = Object.destroy,
+		display = Object.display
+	}
+	return c
+end
+
+Object = {displayed = false, x = 0, z = 0, y = 0, name = "", id="",text = "", imageTag = ""}
+
+function Object:init(x,y,z)
+	self.x = x
+	self.z = z
+	self.y = y
+	self.displayed = false
+end
+
+function Object:setInfos(id,name,text,imageTag)
+	self.id = id
+	self.name = name
+	self.text = text
+	self.imageTag = imageTag
+end
+
+function Object:destroy()
+	local x = self.x
+	local y = self.y
+	local z = self.z
+	digBlock(UpdateQueue,x,y,z)
+
+end
+
+function Object:display()
+	local metaPrimaryColor = E_META_WOOL_LIGHTBLUE
+	local metaSecondaryColor = E_META_WOOL_BLUE
+
+	setBlock(UpdateQueue, self.x, self.y, self.z,E_BLOCK_WOOL,metaPrimaryColor)
+	setBlock(UpdateQueue,self.x,self.y,self.z,E_BLOCK_WALLSIGN,E_META_CHEST_FACING_XP)
+	updateSign(UpdateQueue,self.x,self.y,self.z,"1","2","3","4"£¬2)
+end
+
+
+
