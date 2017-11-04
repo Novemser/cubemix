acd7642ff817beefc2d108aad9646257fac0ff01
naiveee-Q <15757402949@163.com>
2017-11-05 01:28:31 +0800

    repair some bugs

--- fontend/object.lua
+++ fontend/object.lua
@@ -8,7 +8,6 @@ function NewObject()
 		name = "",
 		id = "",
 		text = "",
-		imageTag = "",
 		init = Object.init,
 		setInfos = Object.setInfos,
 		destroy = Object.destroy,
@@ -34,7 +33,7 @@ function Object:destroy()
 	local y = self.y
 	local z = self.z
 	digBlock(UpdateQueue,x,y,z)
-	Object[self.id] = nil
+	Objects[self.id] = nil
 
 end
 
@@ -44,20 +43,14 @@ function Object:display()
 	local objectX={1,2,4,5,1,2,4,5,1,2,4,5}
 	local objectY={2,2,2,2,4,4,4,4,6,6,6,6}
 
-	for i=0,id
-	do
-		if Objects[i] == nil
-		then
-			self.x = objectX+bucketX
-			self.y = Y_GROUND
-			self.z = objectZ+bucketZ
-			setBlock(UpdateQueue, self.x, self.y, self.z,E_BLOCK_BOOKCASE, 0)
-			setBlock(UpdateQueue,self.x,self.y,self.z,E_BLOCK_WALLSIGN,E_META_CHEST_FACING_XP)
-			updateSign(UpdateQueue,self.x,self.y,self.z,"1","2","3","4"£¬2)
-			break
-		end
+	if Objects[self.id] == nil
+	then
+		self.x = objectX[id]+bucketX
+		self.y = Y_GROUND
+		self.z = objectZ[id]+bucketZ
+		setBlock(UpdateQueue, self.x, self.y, self.z,E_BLOCK_BOOKCASE, 0)
+		setBlock(UpdateQueue,self.x,self.y,self.z,E_BLOCK_WALLSIGN,E_META_CHEST_FACING_XP)
+		updateSign(UpdateQueue,self.x,self.y,self.z,self.id,self.name,self.text,"4"ï¼Œ2)
+		break
 	end
 end
-
-
-
